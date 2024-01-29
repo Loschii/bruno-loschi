3ebe53a29121df89aa10323ce860ca395b70c68b
Loschii <bruno.loschi@outlook.com>
2024-01-29 19:15:18 -0300

    primeiro envio de arquivo python para o GitHub

---
+++ aula1.py
@@ -0,0 +1,16 @@
+numero = 42
+print(numero)
+mensagem = "Olá, mundo!"
+print(mensagem)
+x = 5
+y = 3
+soma = x + y
+print(soma)
+esta_chovendo = False
+if esta_chovendo:
+    print("É melhor pegar um guarda-chuva!")
+else:
+    print("O tempo está bom!")
+frutas = ["maçã", "banana", "laranja"]
+for fruta in frutas:
+    print(fruta)
